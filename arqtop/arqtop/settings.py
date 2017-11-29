5558db6a6b5e5d5f7d92eaad5ae51476eaca5d2a
sbenza <silviabenza@gmail.com>
2017-11-28 22:10:54 -0200

    adding django-materials to project

--- arqtop/arqtop/settings.py
+++ arqtop/arqtop/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'material',
+    'material.frontend',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
